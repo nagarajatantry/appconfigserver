89967eb5cf7d2177b3eb03e018960dcc699191da
Nagaraja Tantry <nagaraja_tantry@intuit.com>
2017-10-02 09:52:01 -0700

    initial

---
+++ src/main/java/com/ntantry/poc/appconfigserver/AppconfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.ntantry.poc.appconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppconfigserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ntantry/poc/appconfigserver/AppconfigserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ntantry.poc.appconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppconfigserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
